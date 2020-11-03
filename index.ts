26b1bbf6d68ee21ade72e9c96c0d378a1d642c56
xxc <778994556@qq.com>
2020-11-03 09:33:56 +0800

    update

--- index.ts
+++
@@ -1,8 +0,0 @@
-import App from "./src/App";
-// @ts-ignore
-exports.main = async (context) => {
-    const app = new App(context, "main");
-    return await app.run(async function () {
-        //..
-    });
-}
--- src/dao/ActivityDao.ts
+++
@@ -1,7 +0,0 @@
-import BaseDao from "./abstract/BaseDao";
-
-export default class ActivityDao<T extends object> extends BaseDao<T> {
-    constructor(context) {
-        super(context, "activities");
-    }
-}
