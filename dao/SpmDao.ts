38e2121696783c76e6ca68bb51d635ef37b403f7
xxc <778994556@qq.com>
2020-11-17 10:30:26 +0800

    update spmdao table name

--- dao/SpmDao.ts
+++ dao/SpmDao.ts
@@ -2,6 +2,6 @@ import BaseDao from "./abstract/BaseDao";
 
 export default class SpmDao<T extends object> extends BaseDao<T> {
     constructor(context) {
-        super(context, "spms");
+        super(context, "spm");
     }
 }
